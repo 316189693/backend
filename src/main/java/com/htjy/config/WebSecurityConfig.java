c7ef2ff0791d415addfe75000a58eae4aaa69f1f
will zheng <will.zheng@unisco.com>
2018-12-25 18:45:30 +0800

    modify

--- src/main/java/com/htjy/config/WebSecurityConfig.java
+++ src/main/java/com/htjy/config/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.htjy.config;
 
 
-import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
 
 
