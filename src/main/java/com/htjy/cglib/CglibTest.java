391c8197e64b2b33a65cf80962e8deaf24ca7041
will zheng <will.zheng@unisco.com>
2019-02-19 14:41:22 +0800

    modify

--- src/main/java/com/htjy/cglib/CglibTest.java
+++ src/main/java/com/htjy/cglib/CglibTest.java
@@ -1,9 +1,11 @@
 package com.htjy.cglib;
 
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cglib.proxy.Enhancer;
 
 public class CglibTest {
     public static void main(String[] args) {
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "com/sun/cglib/");
         CglibInterceptor cglibInterceptor = new CglibInterceptor();
 
         Enhancer enhancer = new Enhancer();
