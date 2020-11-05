8fc6f6b3d2ff2eb3b2b7fc8e92124600d0b47a67
willz <316189693@qq.com>
2020-11-05 18:34:08 +0800

    dddd

--- src/main/java/com/htjy/concurrent/locksupport/Test03.java
+++ src/main/java/com/htjy/concurrent/locksupport/Test03.java
@@ -7,20 +7,23 @@ import java.util.concurrent.locks.LockSupport;
 
 public class Test03 {
     static MyContainer<Integer> myContainer = new MyContainer<>();
+    static Thread t1 = null, t2 = null;
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t2 = new Thread(() -> {
+        t2 = new Thread(() -> {
             if (myContainer.size() != 5) {
                 LockSupport.park();
+                LockSupport.unpark(t1);
             }
             System.out.println("Container size:5, t2 close");
         });
-        Thread t1 = new Thread(() -> {
+        t1 = new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 myContainer.add(i);
                 System.out.println("add " + i);
                 if (myContainer.size() == 5) {
                     LockSupport.unpark(t2);
+                    LockSupport.park();
                 }
 
             }
